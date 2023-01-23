118293c0ebaa8df33585280dbac8ebe0ecd6ce47
Okami_PL <szymon.galuszka.114@gmail.com>
2023-01-23 13:43:27 +0100

    lepsze title stron

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -3,17 +3,17 @@ const router = express.Router();
 
 router.get('/', (req, res, next)=>{
     res.render('index', {
-        pageTitle: "Strona główna - PCN",
+        pageTitle: "PCN Strona główna",
     });
 })
 router.get('/about', (req, res, next)=>{
     res.render('about', {
-        pageTitle: "O nas - PCN",
+        pageTitle: "PCN O nas",
     });
 })
 router.get('/contact', (req, res, next)=>{
     res.render('contact', {
-        pageTitle: "Kontakt - PCN",
+        pageTitle: "PCN Kontakt",
     });
 })
 
