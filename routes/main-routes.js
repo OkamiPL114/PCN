c9cc820094b01aec5e85440508f9bf714ebd57ef
Okami_PL <szymon.galuszka.114@gmail.com>
2023-01-23 10:50:53 +0100

    więcej ejs

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next)=>{
-    res.render('index');
+    res.render('index', {
+        pageTitle: "Strona główna - PCN",
+    });
 })
 
 module.exports = router;
