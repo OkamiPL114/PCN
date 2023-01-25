70b1bf2b0293f26d7f60d158e0de146d70927a6c
Okami_PL <szymon.galuszka.114@gmail.com>
2023-01-25 12:19:25 +0100

    parę pakietów do baz danych

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
 
 const mainRoutes = require('./routes/main-routes');
 const errorRoutes = require('./routes/error-routes');
 
 const app = express();
+const port = 3000;
+
+mongoose.connect('mongodb://localhost:27017/pcn')
 
 // zasoby statyczne
 const publicPath = path.join(__dirname, "public");
@@ -14,8 +20,14 @@ app.use(express.static(publicPath));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+// body-parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 // trasy
 app.use(mainRoutes);
 app.use(errorRoutes);
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
